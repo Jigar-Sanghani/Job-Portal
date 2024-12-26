6c04b85fd8d64cb7d95933d8fbc59714f2e6b374
Jigar-Sanghanai <jigarsanghani34@gmail.com>
2024-12-26 15:43:30 +0530

    first

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
-console.log("jigu");
+
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "hello node js" });
+});
 
 
-
-
-console.log("purv");
+const PORT = process.env.PORT || 8090;
+app.listen(PORT, () => {
+    console.log(`Server Is Running On Port ${PORT} !!`);
+});
